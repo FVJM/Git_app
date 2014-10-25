1f69bfffb39e3a1782bf9c22cf0579e6188f5142
fvjm <fvjm@hotmail.com>
2014-10-25 05:59:00 -0500

    miblog_app_v2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ MiblogApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'CategoriaController#index'
+  root 'categoria#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
